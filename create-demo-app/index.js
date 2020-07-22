0a302c1b7251b287f2ec352e5bbcd8a865d1eac2
zhenghaoli <zhenghaoli@corp.netease.com>
2020-07-22 23:40:54 +0800

    feat: index

--- create-demo-app/index.js
+++ create-demo-app/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const commander = require('commander');
 
 var currentNodeVersion = process.versions.node;
 var semver = currentNodeVersion.split('.');
@@ -9,3 +10,16 @@ if (major < 8) {
   console.error('你正在使用node' + currentNodeVersion + '. 请升级至 Node 8 或以上！');
   process.exit(1);
 }
+
+const program = new commander.Command(packageJson.name)
+.version(packageJson.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .action(name => {
+    projectName = name;
+  })
+  .allowUnknownOption()
+  .on('--help', () => {
+    console.log(`一些提示信息。`);
+  })
+  .parse(process.argv);
